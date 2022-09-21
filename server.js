e64013f5c294a3f21b5e03f770696c25ee939a03
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-21 00:47:12 -0500

    create and initalized server in server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`App listening on localhost:${PORT}`);
+
+})
+
+
+
+
+
