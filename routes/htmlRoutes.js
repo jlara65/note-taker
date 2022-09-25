4d9979bc1daafae5f5745e77f571d0298e1103d0
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-25 14:05:52 -0500

    fixed error 'favicon.icon 404 not found by added link in index.html

--- routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = (app) => {
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'))
-        console.log(`You clicked! You're now redirect to notes.html!!`)
-    })
-
-}
