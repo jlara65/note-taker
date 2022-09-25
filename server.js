c00f5661f0101409515d82ecccf4e048f61e060c
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-24 22:41:33 -0500

    fix the missing files from previous commit

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
-const express = require('express');
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+const { notes }  = require("./db/db.json");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-require('./routes/htmlRoutes')(app)
-require('./routes/apiRoutes')(app)
+//Generate Unique ID from install npm module
+const generateUniqueId = require('generate-unique-id');
+
+function createNewNote(body, notesArray) {
+    const note = body; 
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  };  
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = generateUniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});
+    
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const delNote = notes.findIndex(note => note.id ==id);
+
+  notes.splice(delNote, 1);
+  return res.send();
+});
 
 app.listen(PORT, () => {
-    console.log(`App listening on localhost:${PORT}`);
-
-})
-
-
-
-
-
+    console.log(`App listening on PORT ${PORT}`);
+  });
+  
